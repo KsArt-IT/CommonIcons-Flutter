25ab6fd32730acc48edbd52b5a40a1a0002e56b2
KsArT-IT <ksart.it@gmail.com>
2025-06-19 13:31:36 +0300

    feat: add file with class IconAssets

---
+++ lib/icon_assets.dart
@@ -0,0 +1,24 @@
+// GENERATED FILE - DO NOT MODIFY MANUALLY
+// Run `./gen_icons.sh` to regenerate.
+
+class IconAssets {
+  static const _basePath = 'packages/icons/assets/icons';
+
+  static const String vscode = '$_basePath/vscode.svg';
+  static const String linux = '$_basePath/linux.svg';
+  static const String gmail = '$_basePath/gmail.svg';
+  static const String microsoftTodo = '$_basePath/microsoft-todo.svg';
+  static const String flutter = '$_basePath/flutter.svg';
+  static const String git = '$_basePath/git.svg';
+  static const String deepseek = '$_basePath/deepseek.svg';
+  static const String kotlin = '$_basePath/kotlin.svg';
+  static const String dart = '$_basePath/dart.svg';
+  static const String facebook = '$_basePath/facebook.svg';
+  static const String huggingFace = '$_basePath/hugging_face.svg';
+  static const String appleLight = '$_basePath/apple_light.svg';
+  static const String windows = '$_basePath/windows.svg';
+  static const String androidIcon = '$_basePath/android-icon.svg';
+  static const String twitter = '$_basePath/twitter.svg';
+  static const String swift = '$_basePath/swift.svg';
+  static const String appleDark = '$_basePath/apple_dark.svg';
+}
