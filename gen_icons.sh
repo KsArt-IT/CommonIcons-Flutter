27895befb7f209f7afdcb91b637ce16ab0187281
KsArT-IT <ksart.it@gmail.com>
2025-06-19 13:29:49 +0300

    feat: add script for generation

---
+++ gen_icons.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# For generate the 'lib/icon_assets.dart' file from using
+# chmod +x gen_icons.sh
+# ./gen_icons.sh
+echo "⚙️  Generating icons..."
+
+dart lib/src/generate_icon_assets.dart
+
+# Checking the return code
+if [ $? -eq 0 ]; then
+  echo "✅ Ready!"
+else
+  echo "❌ An error occurred during generation."
+fi
